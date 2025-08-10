aa9e279c0816e5406a707fda5e3bc61188cd6d80
Jitprakash <jitprakash2020@gmail.com>
2025-08-10 20:44:42 +0530

    added a base backend structure.

---
+++ Backend/src/test/java/com/code4cause/Qventor/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4cause.Qventor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
