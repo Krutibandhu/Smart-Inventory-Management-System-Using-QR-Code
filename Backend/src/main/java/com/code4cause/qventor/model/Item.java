070f58d1e80a3968e23a6b318c3f7e56087594da
Jitprakash <jitprakash2020@gmail.com>
2025-08-11 22:20:05 +0530

    database connectivity

---
+++ Backend/src/main/java/com/code4cause/qventor/model/Item.java
@@ -0,0 +1,32 @@
+package com.code4cause.qventor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    public Item(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
